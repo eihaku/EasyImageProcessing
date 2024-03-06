2c3945e578754814cdc7e4395b02085b5cee0d28
eihaku <eihaku0205@gmail.com>
2024-03-06 16:30:12 +0900

    一回修正

--- EasyImageProcessing/params.h
+++ EasyImageProcessing/params.h
@@ -7,13 +7,81 @@
 #define LOW         0
 #define LEVEL       256
 
+#define X_IN_POS	280
+#define Y_IN_POS	50
+#define X_OUT_POS	460
+#define Y_OUT_POS	50
+#define X_WORK_POS	370
+#define Y_WORK_POS	210
+#define STDOUT stdout
+#define L_BASE 100           // •º¶¨‚Ž
+#define PI 3.14159265
+#define ROOT2 1.41421356
+#define BUFF_MAX 2500
+#define MAX_X_SIZE 640
+#define BITMAP_HEAD_SIZE         0x036
+#define BITMAP_COLORPALETTE_SIZE 0x400
+
+
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned long  ulong;
+typedef unsigned int   uint;
+
+
+#pragma warning(disable:6031)
+#pragma warning(disable:6064)
+#pragma warning(disable:4996)
+#pragma warning(disable:4244)
+#pragma warning(disable:4305)
+#pragma warning(disable:4715)
+#pragma warning(disable:26451)
+#pragma pack(1)
+struct xyw {
+	int x, y, w;
+}buf[BUFF_MAX];
+
+typedef struct tagBITMAPFILEHEADER {
+	ushort  bfType;
+	uint bfSize;
+	ushort  bfReserved1;
+	ushort  bfReserved2;
+	uint bfOffBits;
+} BITMAPFILEHEADER, * LPBITMAPFILEHEADER, * PBITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER {
+	uint    biSize;
+	uint    biWidth;
+	uint    biHeight;
+	ushort  biPlanes;
+	ushort  biBitCount;
+	uint    biCompression;
+	uint    biSizeImage;
+	uint    biXPelsPerMeter;
+	uint    biYPelsPerMeter;
+	uint    biClrUsed;
+	uint    biClrImportant;
+} BITMAPINFOHEADER, * LPBITMAPINFOHEADER, * PBITMAPINFOHEADER;
+
+extern char bmpHeader[BITMAP_HEAD_SIZE];
+extern char colorPalette[BITMAP_COLORPALETTE_SIZE];
+
+extern PBITMAPFILEHEADER pBmpFileHeader;
+extern PBITMAPINFOHEADER pBmpInfoHeader;
+
+extern unsigned char image_in[Y_SIZE][X_SIZE];
+extern unsigned char image_out[Y_SIZE][X_SIZE];
+extern unsigned char image_work[Y_SIZE][X_SIZE];
+extern unsigned char image_hist[HIST_Y_SIZE][HIST_X_SIZE];
+extern long	hist[LEVEL];
+extern float	ratio[Y_SIZE], size[X_SIZE];
+
 
 void image_read(unsigned char* image, int xsize, int ysize, char* filename);
 void histprint(long hist[]);
-void init_set();
 void image_copy(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZE][X_SIZE]);
 void image_copy2(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZE][X_SIZE]);
-
+void image_write(unsigned char* image, int xsize, int ysize, char* filename);
 void histprint(long hist[]);
 void histgram(unsigned char[Y_SIZE][X_SIZE], long hist[]);
 void histimage(long hist[], unsigned char image_hist[Y_SIZE][X_SIZE]);
@@ -50,15 +118,15 @@ void shift(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZ
 void affine(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZE][X_SIZE], float deg, float zx, float zy, float px, float py);
 void perspect(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZE][X_SIZE], float ax, float xy, float px, float py, float pz, float rz, float rx, float ry, float v, float s);
 void masking(unsigned char image_in[Y_SIZE][X_SIZE], unsigned char image_out[Y_SIZE][X_SIZE], unsigned char image_mask[Y_SIZE][X_SIZE]);
-void gray_display(unsigned char image_in[Y_SIZE][X_SIZE], int xsize, int ysize, int xpos, int ypos);
-void graph_clear();
-void palette8();
-void end_set();
+//void gray_display(unsigned char image_in[Y_SIZE][X_SIZE], int xsize, int ysize, int xpos, int ypos);
+//void graph_clear();
+//void palette8();
+//void end_set();
 int calc_size(unsigned char image_label[Y_SIZE][X_SIZE], int label, int* cx, int* cy);
 float calc_length(unsigned char image_label[Y_SIZE][X_SIZE], int label);
 float trace(unsigned char image_label[Y_SIZE][X_SIZE], int  xs, int ys);
 void matrix(float l[4][4], float m[4][4], float n[4][4]);
 void param_pers(float k[], float a, float b, float x0, float y0, float z0, float z, float x, float y, float t, float s);
-void piel_write(int x, int y, int pale_no);
+//void piel_write(int x, int y, int pale_no);
 //void graph_clear1();
 //void outp(int a, int b);
